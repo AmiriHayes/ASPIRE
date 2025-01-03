a3e80106f0f9cde149b5268722138a366f353238
Amiri Hayes <89553495+AmiriHayes@users.noreply.github.com>
2025-01-03 03:03:32 -0500

    Update res_api.py

--- res_api.py
+++ res_api.py
@@ -2,19 +2,33 @@
 
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-import osmnx
-
 app = Flask(__name__)
-CORS(app)
+
+import json
+import osmnx as ox
+from geopy import geocoders
+G = geocoders.GeoNames(username='amiri_hayes_')
 
 @app.route('/process', methods=['POST'])
 def process_data():
     data = request.json
-    param1 = data.get('param1')
-    param2 = data.get('param2')
-    # Add your processing logic here
-    geojson_obj = {'testkey1': 'testval1'}  # your GeoJSON object
-    regular_json_obj = {'testkey2': 'testval2'}  # your regular JSON object
+    city_neighborhood = data.get('neighborhood')
+    city_name = data.get('city')
+    city_state = data.get('state')
+
+    city = city_neighborhood, city_name, city_state
+    center = G.geocode(', '.join(city))[-1]
+    g = ox.graph_from_point(center_point=center, dist=1000, network_type="walk")
+    g = g.to_undirected()
+
+    nodes, edges = ox.graph_to_gdfs(g) 
+    edges = edges[["geometry"]]
+    geojson_img = json.loads(edges.to_json())
+
+    # processing logic:
+    geojson_obj = geojson_img
+    regular_json_obj = {'testkey2': 'testval2'}
+
     return jsonify({'geojson': geojson_obj, 'json': regular_json_obj})
 
 if __name__ == '__main__':
