c504bfbec1b83c7dbf79bb41aba648cd9ba24a0a
Amiri Hayes <89553495+AmiriHayes@users.noreply.github.com>
2025-01-03 02:46:09 -0500

    Update res_api.py

--- res_api.py
+++ res_api.py
@@ -1,21 +1,20 @@
 # this file is for the demo website: www.aspirefortransport.org
 
-import osmnx
 from flask import Flask, request, jsonify
+from flask_cors import CORS
+import osmnx
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/process', methods=['POST'])
 def process_data():
     data = request.json
-    param1 = data.get('neighborhood_name')
-    param2 = data.get('city_name')
-    param3 = data.get('state_name')
-
-    # processing logic:
-    geojson_obj = {'testkey1': 'testval1'}
-    regular_json_obj = {'testkey2': 'testval2'}
-
+    param1 = data.get('param1')
+    param2 = data.get('param2')
+    # Add your processing logic here
+    geojson_obj = {'testkey1': 'testval1'}  # your GeoJSON object
+    regular_json_obj = {'testkey2': 'testval2'}  # your regular JSON object
     return jsonify({'geojson': geojson_obj, 'json': regular_json_obj})
 
 if __name__ == '__main__':
